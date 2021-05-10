dcd2dabdacc537ca884164011d9a1e17eab8a597
Burhan Sabuwala <52125885+BurhanSabuwala@users.noreply.github.com>
2021-05-10 07:16:05 +0530

    Addedinverse of lower triangular matrix code, inverse of symmetric matrix and cholesky decomposition code.

--- serial_code/matrix_functions.c
+++ serial_code/matrix_functions.c
@@ -88,15 +88,92 @@ void Hadamard(int nrow, int ncol, double A[][ncol], double B[][ncol], double C[]
 			C[i][j] = A[i][j] * B[i][j];
 }
 
+void Cholesky_Decomposition(int nrows, int ncols, double A[][ncols], double L[][ncols], int init_L)
+{
+	if (nrows!=ncols){
+		// Raise error 
+		printf("Input should be a square matrix!");
+	}
+
+	// initalize L
+	// Loops are parallellizable
+	if (init_L==0){
+		for(int i =0; i< nrows; i++)
+			for(int j = 0; j<ncols; j++)
+				L[i][j] = 0;		
+	}
+ 
+    // Decomposing a matrix into Lower Triangular
+    //Loop carried dependency and therefore not parallelizable along one of the loop
+    for (int i = 0; i < nrows; i++) {
+        for (int j = 0; j <= i; j++) {
+            int sum = 0;
+ 
+            if (j == i) // summation for diagnols
+            {
+                for (int k = 0; k < j; k++)
+                    sum += L[j][k]*L[j][k];
+                L[j][j] = sqrt(A[j][j] - sum);
+            } else {
+ 
+                // Evaluating L(i, j) using L(j, j)
+                for (int k = 0; k < j; k++)
+                    sum += (L[i][k] * L[j][k]);
+                L[i][j] = (A[i][j] - sum) / L[j][j];
+            }
+        }
+    }
+}
+
+void inverse_lower_triangular(int nrowL, int ncolL, double L[][ncolL], double Linverse[][ncolL]){
+	if (nrowL!=ncolL){
+		// Raise error 
+		printf("Input should be a square matrix!");
+	}
+	//First two for loops are parallelizable
+	for (int i=0; i<nrowL; i++){
+		for (int j=0; j<=i;j++){
+			if (i==j){
+				Linverse[i][j] = 1/L[i][i];
+			}else{
+				double sum = 0;
+				for(int k =j; k<i;k++){
+					sum += L[i][k]/L[k][j];
+				}
+				Linverse[i][j] = -sum/(L[i][i]);
+			}
+		}
+	}
+}
+
+void inverse_symmetric_matrix(int nrow, int ncol, double A[][ncol], double inv_A[][nrow]){
+	//Check if the matrix is square
+	double L[nrow][ncol];
+	Cholesky_Decomposition(nrow, ncol, A, L, 0);
+
+	// Find Inverse of lower triangular matrix
+	double Linverse[nrow][ncol];
+	inverse_lower_triangular(nrow, ncol, L, Linverse);
+
+	// Find transpose of the inverse of the lower triangular matrix
+	double L_transpose_inverse[nrow][ncol];
+	transpose(nrow, ncol, Linverse, L_transpose_inverse);
+
+	// Multiply to obtain the resulting inverse matrix
+	matmul(nrow, ncol, L_transpose_inverse, ncol, nrow, Linverse, inv_A);
+
+}
+
 int main(int argc, char* argv[]){
 	printf("Hello world!\n");
 	// Check if norm works
+	/*
 	double x[] = {2.1,3.2,1.2,3.2};
 	double y= norm(x, 2);
-	printf("%f\n", y);
+	printf("%f\n", y);*/
 
 	// Hadamard Product
-	int nrow = 2; 
+	int nrow = 5; 
 	int ncol = 3;
 	//double* A = (double*) malloc (nrow*ncol*sizeof( double ));
 	double A[nrow][ncol], B[nrow][ncol], C[nrow][ncol];
@@ -110,8 +187,12 @@ int main(int argc, char* argv[]){
 	print_matrix(ncol, nrow, C_T);
 	double Ctc[ncol][ncol];
 	matmul(ncol, nrow, C_T, nrow, ncol, C, Ctc);
+
+	double L[ncol][ncol]; 
+	int init_flag = 0;
+	Cholesky_Decomposition(ncol,ncol, Ctc, L, init_flag);
 	printf("\n");
-	print_matrix(ncol, ncol, Ctc);
+	print_matrix(ncol, ncol, L);
 
 	return 0;
 }
